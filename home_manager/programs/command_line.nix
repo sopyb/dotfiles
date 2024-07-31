286b5ce2fef45a1184a6c565a6714204f4209a0c
sopy <contact@sopy.one>
2024-07-31 09:41:29 +0300

    update

--- home_manager/programs/command_line.nix
+++ home_manager/programs/command_line.nix
@@ -2,11 +2,11 @@
 
 {
   imports = [
+    ./command_line/archive_utils.nix
     ./command_line/direnv.nix
     # ./command_line/eza.nix
     ./command_line/htop.nix
     ./command_line/hyfetch.nix
-    ./command_line/zip_utils.nix
     ./command_line/zsh.nix
   ];
 
