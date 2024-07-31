c4b8c07c2a1be1157ff3c0b0f5173e1778db8350
sopy <contact@sopy.one>
2024-07-31 12:09:21 +0300

    update

--- home_manager/programs/command_line.nix
+++ home_manager/programs/command_line.nix
@@ -5,6 +5,7 @@
     ./command_line/archive_utils.nix
     ./command_line/direnv.nix
     ./command_line/eza.nix
+    ./command_line/git.nix
     ./command_line/htop.nix
     ./command_line/hyfetch.nix
     ./command_line/zsh.nix
---
+++ home_manager/programs/command_line/git.nix
@@ -0,0 +1,28 @@
+{ ... }:
+
+{
+  programs.git = {
+    enable = true;
+    
+    userName = "Sopy";
+    userEmail = "contact@sopy.one";
+
+    signing = {
+      key = "670B2256BE1A9B17";
+      signByDefault = true;
+    };
+    
+    extraConfig = {
+      core = {
+        autocrlf = "input";
+        editor = "micro";
+      };
+
+      url = {
+        "ssh://git@github.com:" = {
+          insteadOf = "https://github.com/";
+        };
+      };
+    };
+  }
+}
