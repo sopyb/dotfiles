b24e5a956aa8e227a81a8cfd5b21c8cb9785baef
sopy <contact@sopy.one>
2024-08-22 16:40:21 +0300

    I will just buy spotify premium

--- configuration.nix
+++ configuration.nix
@@ -29,7 +29,7 @@
     packages = with pkgs; [
       mpv
       peaclock
-      config.nur.repos.nltch.spotify-adblock
+      # config.nur.repos.nltch.spotify-adblock
       spotify
       protontricks
 
