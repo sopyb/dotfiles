c114f2c830235ab0f534d5710d703764014b155c
Sopy <contact@sopy.one>
2025-01-05 14:21:17 +0200

    add edit alias

--- home_manager/programs/command_line/zsh.nix
+++ home_manager/programs/command_line/zsh.nix
@@ -33,6 +33,11 @@
       POWERLEVEL9K_DISABLE_CONFIGURATION_WIZARD=true
 
       source ~/.config/zsh/.p10k.zsh
+
+
+      function edit() {
+        micro "$(fzf -e --query "$@")"
+      }
     '';
 
     plugins = [
