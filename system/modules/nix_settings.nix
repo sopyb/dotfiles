8884a53b234ebae7be10d0c8d67c1d8f3cb5d014
Sopy <contact@sopy.one>
2025-01-06 20:52:55 +0200

    add lix cache

--- system/modules/nix_settings.nix
+++ system/modules/nix_settings.nix
@@ -6,15 +6,33 @@
       experimental-features = [ "nix-command" "flakes" ];
       trusted-users = [ "root" "@wheel" ];
 
-      substituters = [ "https://cosmic.cachix.org/" ];
-      trusted-public-keys = [ "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE=" ];
+      substituters = [
+        "https://cosmic.cachix.org/"
+        "https://cache.lix.systems"
+        "https://cache.nixos.org/"
+      ];
+      trusted-public-keys = [
+        "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE="
+        "cache.lix.systems:aBnZUw8zA7H35Cz2RyKFVs3H4PlGTLawyY5KRbvJR8o="
+        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      ];
+
+      binary-caches-parallel-connections = 5;
+
+      auto-optimise-store = true;
+    };
+
+
+    gc = {
+      automatic = true;
+      dates = "daily";
+      options = "--delete-older-than 7d";
     };
 
     package = pkgs.lix;
 
     extraOptions = ''
       experimental-features = nix-command flakes
-      binary-caches-parallel-connections = 5
     '';
   };
 }
