9316156db97f543bcffc45fbe5457cf5aa293407
Sopy <contact@sopy.one>
2025-03-08 18:19:13 +0200

    add winVM specializations

---
+++ system/specializations/winVM/disableDGPUspec.nix
@@ -0,0 +1,25 @@
+{ pkgs, ... }:
+
+{
+  specialisation = {
+    disabledDgpu = {
+      inheritParentConfig = true;
+      configuration = {
+
+        config = {
+          system.nixos.tags = [ "passthrough" ];
+          boot.loader.grub.configurationName = ''dGPU Disabled" --class "nixos'';
+
+          # Alphicta
+          boot.initrd.preDeviceCommands = ''
+            DEVS="0000:01:00.0 0000:01:00.1"
+            for DEV in $DEVS; do
+              echo "vfio-pci" > /sys/bus/pci/devices/$DEV/driver_override
+            done
+            modprobe -i vfio-pci
+          '';
+        };
+      };
+    };
+  };
+}
