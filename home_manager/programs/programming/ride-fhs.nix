04b9ef3ba1031a82a3bc62143c1e9d8c12137d3f
Sopy <contact@sopy.one>
2025-03-19 10:07:36 +0200

    test

---
+++ home_manager/programs/programming/ride-fhs.nix
@@ -0,0 +1,13 @@
+{ pkgs, ... }:
+
+(pkgs.buildFHSEnv {
+  name = "rider-env";
+  targetPkgs = pkgs: (with pkgs; [
+    dotnetCorePackages.dotnet_8.sdk
+    dotnetCorePackages.dotnet_8.aspnetcore
+    powershell
+  ]);
+  multiPkgs = pkgs: (with pkgs; [
+  ]);
+  runScript = "nohup rider &";
+}).env
