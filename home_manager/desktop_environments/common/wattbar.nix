c9195e9778e4a950c7c31d2e1aea5612418cec8c
Sopy <contact@sopy.one>
2026-01-26 19:05:29 +0200

    update wattbar theme

--- home_manager/desktop_environments/common/wattbar.nix
+++ home_manager/desktop_environments/common/wattbar.nix
@@ -24,7 +24,11 @@
       100%  #8aadf4	#363a4f  # Blue / Surface0
 
       [nocharge]
-      0%		#939ab7	#181926  # Overlay2 / Crust
+      0%	  #f5bde6	#363a4f  # Pink / Surface0
+      15%	  #eed49f	#363a4f  # Yellow / Surface0
+      40%   #a6da95	#363a4f  # Green / Surface0
+      90%	  #8aadf4	#363a4f  # Blue / Surface0
+      100%  #8aadf4	#363a4f  # Blue / Surface0
     '';
   };
 }
