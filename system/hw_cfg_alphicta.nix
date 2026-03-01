3c50dcecf28fd66e3a0a183b293bf244439df62e
Sopy <contact@sopy.one>
2026-03-01 22:34:39 +0200

    update: tlp settings

--- system/hw_cfg_alphicta.nix
+++ system/hw_cfg_alphicta.nix
@@ -108,18 +108,21 @@
   services.tlp = {
     enable = true;
     settings = {
-      CPU_SCALING_GOVERNOR_ON_AC = "powersave";
+      CPU_DRIVER_OPMODE_ON_AC = "active";
+      CPU_DRIVER_OPMODE_ON_BAT = "guided";
+
+      CPU_SCALING_GOVERNOR_ON_AC = "performance";
       CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
 
-      CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
       CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+      CPU_ENERGY_PERF_POLICY_ON_BAT = "balance_power";
 
       CPU_MIN_PERF_ON_AC = 0;
       CPU_MAX_PERF_ON_AC = 100;
       CPU_MIN_PERF_ON_BAT = 0;
-      CPU_MAX_PERF_ON_BAT = 20;
+      CPU_MAX_PERF_ON_BAT = 60;
 
-      CPU_BOOST_ON_AC = 0;
+      CPU_BOOST_ON_AC = 1;
       CPU_BOOST_ON_BAT = 0;
     };
   };
