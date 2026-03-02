8665bf88551aa76363ad6a13bb1c7147b262bfb0
Sopy <contact@sopy.one>
2026-03-02 16:00:50 +0200

    update: disable lightdm display manager in XFCE configuration

--- system/modules/desktop/desktop_environments/xfce.nix
+++ system/modules/desktop/desktop_environments/xfce.nix
@@ -2,6 +2,6 @@
 
 {
   services.xserver.enable = true;
-  services.xserver.displayManager.lightdm.enable = true;
+  services.xserver.displayManager.lightdm.enable = false;
   services.xserver.desktopManager.xfce.enable = true;
 }
