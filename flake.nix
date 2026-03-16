ee1a743c7135879a6021497215fefdd74719379b
Sopy <contact@sopy.one>
2026-03-17 01:11:55 +0200

    update(flake): pin nixpkgs so electron builds

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "A very basic flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/0c46193f275d8fdfe9af421ab541fead2d0ab276";
     nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-25.05";
 
     home-manager = {
