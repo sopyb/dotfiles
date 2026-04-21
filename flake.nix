ef40abf8d98c878f556b2fa54721c93c61e7e1b8
Sopy <contact@sopy.one>
2026-04-22 00:03:53 +0300

    update(flake): update inputs

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "A very basic flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-25.05";
 
     nur.url = "github:nix-community/NUR";
