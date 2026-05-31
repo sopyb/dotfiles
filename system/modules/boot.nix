b7b1570b4fd3dd86db09cc714c8d22be606c35eb
Sopy <contact@sopy.one>
2026-05-31 20:33:28 +0300

    undo temp fix(boot): Nvidia builds on 7.0 now

--- system/modules/boot.nix
+++ system/modules/boot.nix
@@ -46,7 +46,7 @@
         efi.efiSysMountPoint = "/boot/EFI";
       };
 
-      kernelPackages = lib.mkDefault pkgs.linuxPackages_6_18;
+      kernelPackages = lib.mkDefault pkgs.linuxPackages_latest;
 
       plymouth = {
         enable = true;
