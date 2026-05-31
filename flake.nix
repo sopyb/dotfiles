966252aa9744901741280badc1b35a515e849691
Sopy <contact@sopy.one>
2026-05-31 20:46:34 +0300

    update(flake): nixpkgs-stable to 26.05

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-25.05";
+    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-26.05";
 
     nur.url = "github:nix-community/NUR";
 
