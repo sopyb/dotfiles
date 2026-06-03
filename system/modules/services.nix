700ea6808e80965f8dca462179c77cdae3b7dcaa
Sopy <contact@sopy.one>
2026-06-03 13:44:28 +0300

    update(services): add wivrn (OMFG YOU CAN VR ON LINUGZ)

--- system/modules/services.nix
+++ system/modules/services.nix
@@ -1,5 +1,9 @@
 { pkgs, ... }:
 
+let
+  adb = "${pkgs.android-tools}/bin/adb";
+  lsusb = "${pkgs.usbutils}/bin/lsusb";
+in
 {
   # fix for Mullvad VPN
   networking.networkmanager.enable = true;
@@ -25,6 +29,7 @@
     udev = {
       extraRules = ''
         SUBSYSTEM=="kvmfr", OWNER="sopy", GROUP="kvm", MODE="0660"
+        ACTION=="add", SUBSYSTEM=="usb", ATTR{idVendor}=="2833", TAG+="systemd", ENV{SYSTEMD_WANTS}="wivrn-adb-tunnel.service"
       '';
     };
 
@@ -40,7 +45,31 @@
     };
 
     fwupd.enable = true;
+
+    
+      wivrn = {
+        enable = true;
+        openFirewall = true;
+
+        autoStart = true;
+
+        package = (pkgs.wivrn.override { cudaSupport = true; });
+      };
   };
+
+  systemd.services."wivrn-adb-tunnel" = {
+      description = "WiVRn ADB reverse tunnel (used by udev)";
+      serviceConfig = {
+        Type = "oneshot";
+        ExecStartPre = "${pkgs.coreutils}/bin/sleep 3";
+        ExecStart = pkgs.writeShellScript "wivrn-adb-tunnel" ''
+          set -euo pipefail
+          serial=$(${lsusb} -vd 2833: | grep iSerial | awk '{print $3}' | head -n1 || true)
+          [ -n "$serial" ] || { echo "device not found"; exit 1; }
+          ${adb} -s "$serial" reverse tcp:9757 tcp:9757
+        '';
+      };
+    };
 
 
   security = {
