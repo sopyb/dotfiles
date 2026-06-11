1964a8188566bc63c025695132aece915451dc00
Sopy <contact@sopy.one>
2026-06-11 10:53:23 +0300

    chore(fan-max.sh): clean-up script

--- system/machines/alphicta/fan-max.sh
+++ system/machines/alphicta/fan-max.sh
@@ -10,18 +10,10 @@ readonly EC_MEM="/sys/kernel/debug/ec/ec0/io"
 
 readonly EC_BIOS_CTRL=0x62
 readonly EC_TIMER=0x63
-readonly EC_FAN_STATE=0xF4
 readonly EC_PERFORMANCE=0x95
-readonly EC_FAN_BOOST=0xEC
 
 readonly EC_FAN1_RPM=0x34
-readonly EC_FAN1_PCT=0x2E
-
 readonly EC_FAN2_RPM=0x35
-readonly EC_FAN2_PCT=0x2F
-
-readonly EC_CPU_TEMP=0x57
-readonly EC_GPU_TEMP=0xB7
 
 ec_write() {
     printf "\\x$(printf '%02x' $2)" | dd of="$EC_MEM" bs=1 count=1 seek=$(($1)) conv=notrunc 2>/dev/null
@@ -36,10 +28,10 @@ restore() {
 
 trap restore SIGINT SIGTERM
 
-ec_write "$EC_BIOS_CTRL" 0x06
-ec_write "$EC_PERFORMANCE" 0x01
-
 while true; do
+    ec_write "$EC_BIOS_CTRL" 0x06
+    ec_write "$EC_PERFORMANCE" 0x01
+
     ec_write "$EC_FAN1_RPM" "$FAN1_MAX"
     ec_write "$EC_FAN2_RPM" "$FAN2_MAX"
     ec_write "$EC_TIMER" 0x78
