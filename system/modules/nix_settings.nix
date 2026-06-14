679223790a74a9908188277ec588fa07155c943f
Sopy <contact@sopy.one>
2026-06-14 09:51:56 +0300

    update(nix): add noctalia binary cache

--- system/modules/nix_settings.nix
+++ system/modules/nix_settings.nix
@@ -10,12 +10,14 @@
         "https://cache.lix.systems"
         "https://cache.nixos.org/"
         "https://hyprland.cachix.org"
+        "https://noctalia.cachix.org"
       ];
 
       trusted-public-keys = [
         "cache.lix.systems:aBnZUw8zA7H35Cz2RyKFVs3H4PlGTLawyY5KRbvJR8o="
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
         "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+        "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4="
       ];
 
       binary-caches-parallel-connections = 10;
