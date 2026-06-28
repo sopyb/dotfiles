db0f8ba97b06171a46d57c2552c6ec63744c9ac7
Sopy <contact@sopy.one>
2026-06-28 23:07:29 +0300

    update(lamsurrus): add VM config and disable audio services

--- system/machines/lamsurrus/default.nix
+++ system/machines/lamsurrus/default.nix
@@ -1,5 +1,18 @@
-{ ... }:
+{ lib, ... }:
 
 {
-  # add vm configs here
+  imports = [
+    ../../modules/boot.nix
+  ];
+
+  virtualisation.vmVariant = {
+    virtualisation = {
+      diskSize = 32 * 1024;
+      memorySize = 16 * 1024;
+      cores = 8;
+    };
+  };
+
+  # disable pipewire and pulseaudio so no audio can be played from the vm
+  services.pipewire.enable = lib.mkForce false;
 }
