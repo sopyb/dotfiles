cdcbdd239a08de86d1cc07670e3f3f0416781458
Sopy <contact@sopy.one>
2026-07-15 14:57:14 +0300

    update(vesktop): move vesktop to packages

--- home_manager/programs/communication.nix
+++ home_manager/programs/communication.nix
@@ -1,11 +1,9 @@
 { pkgs, ... }:
 
-let
-  vesktop = import ./communication/vesktop.nix { inherit pkgs; };
-in
 {
   home.packages = with pkgs; [
     custom.commet-chat
+    custom.vesktop
     element-desktop
     teams-for-linux
     thunderbird
--- packages/default.nix
+++ packages/default.nix
@@ -4,6 +4,7 @@
   commet-chat = pkgs.callPackage ./commet-chat.nix { };
   davinci-resolve-studio = pkgs.callPackage ./davinci-resolve-studio.nix { };
   wattbar = pkgs.callPackage ./wattbar.nix { };
+  vesktop = pkgs.callPackage ./vesktop.nix { };
 
   # thesis
   kitty = pkgs.callPackage ./tap/kitty.nix { };
