3f603a58bff4f4b64eeb0988e8070490117d20de
Sopy <contact@sopy.one>
2026-07-23 22:58:04 +0300

    update(home): add  games and torrents userdirs

--- home/programs/base/default.nix
+++ home/programs/base/default.nix
@@ -1,13 +1,35 @@
-{ self, ... }:
+{ self, config, ... }:
 
 {
   imports = [
     (self + /home/programs/command_line)
   ];
 
-  config.home = {
-    stateVersion = "24.05";
-    username = "sopy";
-    homeDirectory = "/home/sopy";
+  config = {
+    home = {
+      stateVersion = "24.05";
+      username = "sopy";
+      homeDirectory = "/home/sopy";
+    };
+
+    xdg.userDirs = {
+      enable = true;
+      createDirectories = true;
+      setSessionVariables = true;
+
+      desktop = "${config.home.homeDirectory}/Desktop";
+      documents = "${config.home.homeDirectory}/Documents";
+      download = "${config.home.homeDirectory}/Downloads";
+      music = "${config.home.homeDirectory}/Music";
+      pictures = "${config.home.homeDirectory}/Pictures";
+      videos = "${config.home.homeDirectory}/Videos";
+      publicShare = "${config.home.homeDirectory}/Public";
+      templates = "${config.home.homeDirectory}/Templates";
+
+      extraConfig = {
+        games = "${config.home.homeDirectory}/Games";
+        torrents = "${config.home.homeDirectory}/Torrents";
+      };
+    };
   };
 }
