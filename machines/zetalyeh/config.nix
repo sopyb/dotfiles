b66e8a3ce4bbd01417dc051ed2a825525de3f61d
Sopy <contact@sopy.one>
2026-09-08 14:56:51 +0300

    update(zetalyeh): fix for new options

--- machines/zetalyeh/config.nix
+++ machines/zetalyeh/config.nix
@@ -2,12 +2,30 @@
   machine = {
     name = "zetalyeh";
     type = "hybrid";
-    features = { };
     desktopEnvironment = {
       enable = true;
       types = [ "plasma" ];
       displayManager = "sddm";
     };
-    variables = { };
+    features = {
+      ollama = true;
+      sshd = true;
+      sunshine = true;
+      virtualDisplay = true;
+      virtualization = false;
+    };
+    tweaks = {
+      autoLogin = true;
+      noFirewall = true;
+      noSleep = true;
+    };
+    specializations = {
+      deckmode = false;
+      noDedicatedGPU = false;
+    };
+    variables = {
+      gitSigningKey = "";
+      gitSigning = false;
+    };
   };
 }
