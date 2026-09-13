26ca7b895447040c7a622c19c354a3e07ac5e5d2
Sopy <contact@sopy.one>
2026-09-13 12:42:56 +0300

    update(zetalyeh): add passthrough vps vpn

--- machines/zetalyeh/default.nix
+++ machines/zetalyeh/default.nix
@@ -87,6 +87,30 @@
 
   swapDevices = [ ];
 
+  # setup wireguard
+  networking.wireguard.interfaces.wg0 = {
+    ips = [ "10.10.0.2/24" ];
+    privateKeyFile = "/var/lib/secrets/wireguard/private.key";
+    peers = [{
+      publicKey = "1aSWFrOHqqLRX6IUvB4NQjeinBZQT6lHh+02ZUaf2FY=";
+      endpoint = "130.61.209.240:51820";
+      allowedIPs = [ "0.0.0.0/0" ];
+      persistentKeepalive = 25;
+    }];
+
+    postSetup = ''
+      ip route replace default via 10.10.0.1 dev wg0 table 200 || true
+      ip rule add from 10.10.0.2 table 200 || true
+    '';
+
+    postShutdown = ''
+      ip rule del from 10.10.0.2 table 200 || true
+      ip route del default via 10.10.0.1 dev wg0 table 200 || true
+    '';
+
+    allowedIPsAsRoutes = false;
+  };
+
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
