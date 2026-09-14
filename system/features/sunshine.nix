1e05af779bf87f223e672415a81a775ce713d91d
Sopy <contact@sopy.one>
2026-09-14 11:13:06 +0300

    fix(sunshine): now you will stop yelling at me about cap_sys_nice 🥺

--- system/features/sunshine.nix
+++ system/features/sunshine.nix
@@ -14,4 +14,11 @@
       };
     };
   };
+
+  security.wrappers.sunshine = {
+    owner = "root";
+    group = "root";
+    capabilities = "cap_sys_admin+p";
+    source = "${pkgs.sunshine}/bin/sunshine";
+  };
 }
