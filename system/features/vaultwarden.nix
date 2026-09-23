72740c525afac29faf3321817b1d7aa0418ca5c0
Sopy <contact@sopy.one>
2026-09-24 00:39:23 +0300

    update(vaultwarden): Mutiple changes
    - Add commets with variables filled in createSecretFiles
    - Allow login with Master Password
    - Setup push notifications
    - Move data folder

--- system/features/vaultwarden.nix
+++ system/features/vaultwarden.nix
@@ -22,19 +22,21 @@
       configureNginx = true;
       configurePostgres = true;
       domain = "vault.${config.machine.variables.nginx.domain}";
-      # backupDir = config.machine.variables.paths.vaultwarden;
       environmentFile = "/var/lib/secrets/vaultwarden";
       config = {
         SIGNUPS_ALLOWED = false;
+        # ADMIN_TOKEN = /var/lib/secrets/vaultwarden
 
         ROCKET_ADDRESS = "127.0.0.1";
         ROCKET_PORT = 8222;
         ROCKET_LOG = "critical";
 
         SSO_ENABLED = config.machine.features.authentik;
-        SSO_ONLY = config.machine.features.authentik;
         SSO_ALLOW_UNKNOWN_EMAIL_VERIFICATION = true;
-        SSO_SCOPES = "email profile";
+        SSO_SCOPES = "email profile offline_access";
+        # SSO_AUTHORITY = /var/lib/secrets/vaultwarden
+        # SSO_CLIENT_ID = /var/lib/secrets/vaultwarden
+        # SSO_CLIENT_SECRET = /var/lib/secrets/vaultwarden
 
         SMTP_HOST = "smtp.purelymail.com";
         SMTP_PORT = 587;
@@ -42,9 +44,17 @@
         SMTP_FROM = "vault@sopy.one";
         SMTP_FROM_NAME = "Vaultwarden";
         SMTP_USERNAME = "vault@sopy.one";
+        # SMTP_PASSWORD = /var/lib/secrets/vaultwarden
+
+        PUSH_ENABLED = false;
+        PUSH_RELAY_URI = "https://api.bitwarden.eu";
+        PUSH_IDENTITY_URI = "https://identity.bitwarden.eu";
+        # PUSH_INSTALLATION_ID = /var/lib/secrets/vaultwarden
+        # PUSH_INSTALLATION_KEY = /var/lib/secrets/vaultwarden
+
+        DATA_FOLDER = config.machine.variables.paths.vaultwarden;
       };
     };
-
 
     nginx.virtualHosts."${config.services.vaultwarden.domain}" = lib.mkIf config.machine.variables.nginx.ssl {
       forceSSL = true;
