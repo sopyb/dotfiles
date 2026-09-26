7f57663f5a46012eb30f5ed3666cf956bb8b6253
Sopy <contact@sopy.one>
2026-09-26 10:25:10 +0300

    update(networking): add machines to hosts file

--- system/modules/features/networking.nix
+++ system/modules/features/networking.nix
@@ -4,6 +4,14 @@
   networking = {
     enableIPv6 = true;
 
+    extraHosts = builtins.concatStringsSep "\n" [
+      "192.168.1.133 alphicta.sopy.one"
+      "192.168.1.135 zetalyeh.sopy.one"
+
+      "130.61.169.33 omegantes.sopy.one"
+      "  5.45.96.148 yotemen.sopy.one"
+    ];
+
     wireless.iwd.enable = true;
     networkmanager = {
       enable = true;
