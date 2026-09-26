bfc76d6cdf3b7b43269fce42b22d35b65208dcdd
Sopy <contact@sopy.one>
2026-09-26 10:29:49 +0300

    update(omegantes): add nginx

--- machines/omegantes/config.nix
+++ machines/omegantes/config.nix
@@ -2,10 +2,16 @@
   machine = {
     name = "omegantes";
     type = "server";
+    desktopEnvironment.enable = false;
     features = {
+      nginx = true;
       sshd = true;
     };
-    desktopEnvironment.enable = false;
-    variables = { };
+    variables = {
+      nginx = {
+        domain = "sopy.one";
+        ssl = true;
+      };
+    };
   };
 }
